1293b49c37e29b3218a8368ab32a2628e640693d
Pratick <69669215+Pratick-AT@users.noreply.github.com>
2023-05-21 15:36:11 +0530

    Delete chatbot.py

--- chatbot.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-def answer_question(question, context):
-    qa_pipeline = pipeline("question-answering")
-    result = qa_pipeline(question=question, context=context)
-    return result["answer"]
